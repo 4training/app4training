c659deba9677329943a41039379a83c178404063
holybiber <samuel@holydevelopers.net>
2023-09-12 11:52:17 +0200

    Refactor SettingsPage
    Use Widgets instead of helper functions
    (see https://www.youtube.com/watch?v=IOyq-eTRhvo)
    Put the languages table into a separate class/file

--- lib/routes/settings_page.dart
+++ lib/routes/settings_page.dart
@@ -3,13 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:four_training/data/globals.dart';
 import 'package:four_training/l10n/l10n.dart';
 import 'package:four_training/widgets/check_now_button.dart';
+import 'package:four_training/widgets/dropdownbutton_theme.dart';
+import 'package:four_training/widgets/languages_table.dart';
 import '../data/languages.dart';
-import '../widgets/checkbox_download_language.dart';
-import '../widgets/delete_language_button.dart';
-import '../widgets/download_language_button.dart';
 import '../widgets/dropdownbutton_app_language.dart';
 import '../widgets/dropdownbutton_check_frequency.dart';
-import '../widgets/update_language_button.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -22,172 +20,68 @@ class SettingsPage extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
             child: Column(children: [
-          _getAppearance(context),
+          // Set app language
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(context.l10n.appLanguage,
+                  style: Theme.of(context).textTheme.bodyMedium),
+              const DropdownButtonAppLanguage(),
+            ],
+          ),
+          const SizedBox(height: 10),
           const LanguageSettings(),
           const UpdateSettings()
+          // const SizedBox(height: 10),
+          // const DesignSettings()
         ])),
       ),
     );
   }
-
-  Widget _getAppearance(BuildContext context) {
-    List<Widget> widgets = [];
-
-    widgets.add(Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(context.l10n.appLanguage,
-                style: Theme.of(context).textTheme.bodyMedium),
-            const DropdownButtonAppLanguage(),
-          ],
-        )));
-
-    /* This will be part of a later version of the app
-    widgets.add(Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(context.l10n.theme, style: Theme.of(ctx).textTheme.bodyMedium),
-            const DropDownButtonTheme(),
-          ],
-        )));
-     */
-
-    return Column(children: widgets);
-  }
 }
 
+/// All settings regarding languages.
+/// The main part of this is in [LanguagesTable]
 class LanguageSettings extends ConsumerWidget {
   const LanguageSettings({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<Widget> widgets = [];
-
-    widgets.add(Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: Row(children: [
-          Text(context.l10n.languages,
-              style: Theme.of(context).textTheme.titleLarge)
-        ])));
-
-    widgets.add(
-      Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Text(
-            context.l10n.languagesText,
-            style: Theme.of(context).textTheme.bodyMedium,
-          )),
-    );
-
-    List<TableRow> rows = [];
-
-    // Add a table row for each language
-    for (var languageCode in Globals.availableLanguages) {
-      LanguageController lang =
-          ref.watch(languageProvider(languageCode).notifier);
-      Language language = ref.watch(languageProvider(languageCode));
-
-      // TODO read the language specifics
-
-      rows.add(TableRow(children: [
-        Container(
-            height: 32,
-            alignment: Alignment.center,
-            child: CheckBoxDownloadLanguage(languageCode: languageCode)),
-        Container(
-            height: 32,
-            alignment: Alignment.centerLeft,
-            child: Text(languageCode.toUpperCase(),
-                style: Theme.of(context).textTheme.bodyMedium)),
-        Container(
-            height: 32,
-            alignment: Alignment.centerLeft,
-            child: lang.downloaded && lang.updatesAvailable
-                ? UpdateLanguageButton(language: language, callback: () {})
-                : const Text("")),
-        Container(
-            height: 32,
-            alignment: Alignment.centerLeft,
-            child: lang.downloaded
-                ? DownloadLanguageButton(
-                    languageCode: languageCode, callback: () {})
-                : DeleteLanguageButton(
-                    languageCode: languageCode,
-                  )),
-      ]));
-    }
-
-    // Add the table to the widget tree
-    widgets.add(Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Table(
-        //border: TableBorder.all(color: Colors.black26),
-        columnWidths: const {
-          0: IntrinsicColumnWidth(),
-          1: FlexColumnWidth(),
-          2: IntrinsicColumnWidth(),
-          3: IntrinsicColumnWidth(),
-        },
-        children: rows,
-      ),
-    ));
-
     // TODO move this calculation somewhere else?
     int sizeInKB = 0;
     for (String langCode in Globals.availableLanguages) {
       sizeInKB += ref.watch(languageProvider(langCode)).sizeInKB;
     }
 
-    widgets.add(
-      Padding(
-          padding: const EdgeInsets.only(bottom: 10),
+    return Column(children: [
+      Align(
+          alignment: Alignment.topLeft,
           child: Text(
-            "${context.l10n.diskUsage}: $sizeInKB kB",
-            style: Theme.of(context).textTheme.bodyMedium,
+            context.l10n.languages,
+            style: Theme.of(context).textTheme.titleLarge,
           )),
-    );
-
-    return Column(children: widgets);
+      const SizedBox(height: 10),
+      Text(
+        context.l10n.languagesText,
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+      const SizedBox(height: 10),
+      const LanguagesTable(),
+      Text(
+        "${context.l10n.diskUsage}: $sizeInKB kB",
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+      const SizedBox(height: 10),
+    ]);
   }
 }
 
+/// All settings about checking for updates
 class UpdateSettings extends ConsumerWidget {
   const UpdateSettings({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<Widget> widgets = [];
-
-    widgets.add(Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: Row(children: [
-          Text(context.l10n.automaticUpdate,
-              style: Theme.of(context).textTheme.titleLarge)
-        ])));
-
-    widgets.add(Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(context.l10n.updateText,
-                style: Theme.of(context).textTheme.bodyMedium),
-          ],
-        )));
-
-    widgets.add(const Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            DropdownButtonCheckFrequency(),
-          ],
-        )));
-
     // TODO
     // DateTime lastCheck = ref.watch(updatesAvailableProvider);
     // Convert into human readable string in local time
@@ -195,22 +89,55 @@ class UpdateSettings extends ConsumerWidget {
     // String timestamp = DateFormat('yyyy-MM-dd HH:mm').format(localTime);
     String timestamp = 'TODO see #87';
 
-    widgets.add(Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Text("${context.l10n.lastTime} ",
+    return Column(children: [
+      Align(
+          alignment: Alignment.topLeft,
+          child: Text(context.l10n.automaticUpdate,
+              style: Theme.of(context).textTheme.titleLarge)),
+      const SizedBox(height: 10),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(context.l10n.updateText,
               style: Theme.of(context).textTheme.bodyMedium),
-          Text(timestamp, style: Theme.of(context).textTheme.bodyMedium)
-        ])));
+        ],
+      ),
+      const SizedBox(height: 10),
+      const Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          DropdownButtonCheckFrequency(),
+        ],
+      ),
+      const SizedBox(height: 10),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Text("${context.l10n.lastTime} ",
+            style: Theme.of(context).textTheme.bodyMedium),
+        Text(timestamp, style: Theme.of(context).textTheme.bodyMedium)
+      ]),
+      const SizedBox(height: 10),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CheckNowButton(buttonText: context.l10n.checkNow, callback: () {})
+        ],
+      )
+    ]);
+  }
+}
 
-    widgets.add(Row(
-      mainAxisAlignment: MainAxisAlignment.end,
+/// TODO select light / dark theme
+class DesignSettings extends StatelessWidget {
+  const DesignSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        CheckNowButton(buttonText: context.l10n.checkNow, callback: () {})
+        Text(context.l10n.theme, style: Theme.of(context).textTheme.bodyMedium),
+        const DropDownButtonTheme(),
       ],
-    ));
-
-    return Column(children: widgets);
+    );
   }
 }
---
+++ lib/widgets/languages_table.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:four_training/data/globals.dart';
+import 'package:four_training/data/languages.dart';
+import 'package:four_training/widgets/checkbox_download_language.dart';
+import 'package:four_training/widgets/delete_language_button.dart';
+import 'package:four_training/widgets/download_language_button.dart';
+import 'package:four_training/widgets/update_language_button.dart';
+
+/// For the settings page: Show table with list of available languages.
+/// Features:
+/// - select which of them should be available offline
+/// - delete language files
+/// - download language files (if not yet downloaded)
+/// - update language if updates are available
+class LanguagesTable extends ConsumerWidget {
+  const LanguagesTable({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<TableRow> rows = [];
+
+    // Add a table row for each language
+    for (var languageCode in Globals.availableLanguages) {
+      LanguageController lang =
+          ref.watch(languageProvider(languageCode).notifier);
+      Language language = ref.watch(languageProvider(languageCode));
+      rows.add(TableRow(children: [
+        Container(
+            height: 32,
+            alignment: Alignment.center,
+            child: CheckBoxDownloadLanguage(languageCode: languageCode)),
+        Container(
+            height: 32,
+            alignment: Alignment.centerLeft,
+            child: Text(languageCode.toUpperCase(),
+                style: Theme.of(context).textTheme.bodyMedium)),
+        Container(
+            height: 32,
+            alignment: Alignment.centerLeft,
+            child: lang.downloaded && lang.updatesAvailable
+                ? UpdateLanguageButton(language: language, callback: () {})
+                : const Text("")),
+        Container(
+            height: 32,
+            alignment: Alignment.centerLeft,
+            child: lang.downloaded
+                ? DownloadLanguageButton(
+                    languageCode: languageCode, callback: () {})
+                : DeleteLanguageButton(
+                    languageCode: languageCode,
+                  )),
+      ]));
+    }
+
+    return Table(
+      //border: TableBorder.all(color: Colors.black26),
+      columnWidths: const {
+        0: IntrinsicColumnWidth(),
+        1: FlexColumnWidth(),
+        2: IntrinsicColumnWidth(),
+        3: IntrinsicColumnWidth(),
+      },
+      children: rows,
+    );
+  }
+}
